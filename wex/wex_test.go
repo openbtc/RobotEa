46dfa87d6c42eed6576e8a78b5de1b0e4acc508b
openbtc <openbtc@outlook.com>
2018-01-22 00:38:00 +0800

    [wex.nz] ticker

---
+++ wex/wex_test.go
@@ -0,0 +1,15 @@
+package wex
+
+import (
+	"net/http"
+	"testing"
+	"github.com/openbtc/RobotEa"
+)
+
+var wex = New(http.DefaultClient, "", "")
+
+func TestWex_GetTicker(t *testing.T) {
+	ticker, err := wex.GetTicker(goex.BTC_USD)
+	t.Log("err=>", err)
+	t.Log("ticker=>", ticker)
+}
