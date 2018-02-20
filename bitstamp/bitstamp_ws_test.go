fb5b16a34e51c66466e15f79c3279f14f6ff3ae7
openbtc <openbtc@outlook.com>
2018-02-20 13:28:01 +0800

    [bitstamp.net] order book with websocket

---
+++ bitstamp/bitstamp_ws_test.go
@@ -0,0 +1,18 @@
+package bitstamp
+
+import (
+	"github.com/openbtc/RobotEa"
+	"testing"
+	"time"
+	"log"
+)
+
+func TestBitstamp_GetDepthWithWs(t *testing.T) {
+	btmp.GetDepthWithWs(goex.BCH_USD, func(depth *goex.Depth) {
+		log.Println(depth)
+	})
+	btmp.GetDepthWithWs(goex.LTC_USD , func(depth *goex.Depth) {
+		log.Println(depth)
+	})
+	time.Sleep(1 * time.Minute)
+}
